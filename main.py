0722b71e77590dca5c2ff52edaeefcd87a60725e
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2024-02-08 02:06:29 +0800

    Feat: Add custom shorten URL
    Modify API path

--- main.py
+++ main.py
@@ -4,23 +4,43 @@ import uvicorn
 import random
 import model
 
-app = FastAPI()
+app = FastAPI(root_path='/api/v1/shorten-url')
 
+
+# @app.get('/{id}')
+# async def redirect(id):
+#     result = await model.find({'shorten_url': id})
+#     if result == None:
+#         raise HTTPException(status_code=404)
+#     return RedirectResponse(f'{result["full_url"]}')
 
 @app.get('/{id}')
-async def redirect(id):
+async def get_full_url_code(id):
     result = await model.find({'shorten_url': id})
     if result == None:
         raise HTTPException(status_code=404)
-    return RedirectResponse(f'{result["full_url"]}')
+    return {'full_url': result["full_url"]}
 
-@app.post('/shorten-url')
-async def add_shorten_url(full_url: str):
+@app.post('/')
+async def add_shorten_url_code(full_url: str, shorten_url: str | None = None):
+    # if shorten_url is specified
+    if shorten_url != None:
+        result = await model.find({'shorten_url': shorten_url})
+        if result != None:
+            return {'shorten_url_code': result['shorten_url']}
+        else:
+            await model.insert({
+                'shorten_url': shorten_url, 
+                'full_url': full_url
+            })
+            # return the shorten url code
+            return {'shorten_url_code': shorten_url}
+    # else: if shorten_url is not specified
     # search for full_url 
     result = await model.find({'full_url': full_url})
     # full_url exist
     if result != None:
-        return result.shorten_url
+        return {'shorten_url_code': result['shorten_url']}
     # full_url not exist
     # all characters can be used in shorten_url code
     characters = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
@@ -44,7 +64,7 @@ async def add_shorten_url(full_url: str):
         'full_url': full_url
     })
     # return the shorten url code
-    return {'shorten_url': f'/{shorten_url}'}
+    return {'shorten_url_code': shorten_url}
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='127.0.0.1', port=8003, reload=True)
