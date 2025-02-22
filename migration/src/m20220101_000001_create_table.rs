d016f27221698d5de1f7e616c6a0bc7bf85b295f
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-02-22 20:39:29 +0800

    🔨 implement migration script

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -6,36 +6,30 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(ShortenUrl::Table)
                     .if_not_exists()
-                    .col(pk_auto(Post::Id))
-                    .col(string(Post::Title))
-                    .col(string(Post::Text))
+                    .col(pk_auto(ShortenUrl::Id))
+                    .col(string(ShortenUrl::ShortenId))
+                    .col(string(ShortenUrl::OriginalUrl))
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(ShortenUrl::Table).to_owned())
             .await
     }
 }
 
 #[derive(DeriveIden)]
-enum Post {
+enum ShortenUrl {
     Table,
     Id,
-    Title,
-    Text,
+    ShortenId,
+    OriginalUrl,
 }
