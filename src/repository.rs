480af29b27fab454b22e85fcf5a7dea67fa0ee91
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-02-22 23:51:56 +0800

    🐛 type errors

--- src/repository.rs
+++ src/repository.rs
@@ -1,5 +1,5 @@
-use crate::entities::{prelude::*, shorten_url};
-use sea_orm::{DatabaseConnection, EntityTrait, QueryFilter};
+use crate::entities::{prelude::*, shorten_url::ActiveModel, *};
+use sea_orm::{ActiveValue, ColumnTrait, DatabaseConnection, DbErr, EntityTrait, QueryFilter};
 
 #[derive(Debug, Clone)]
 pub struct ShortenUrlRepository {
@@ -8,63 +8,75 @@ pub struct ShortenUrlRepository {
 
 impl ShortenUrlRepository {
     /// Find a record with the same shorten ID
-    pub async fn find_by_shorten_id(&self, shorten_id: &str) -> Result<shorten_url::Model> {
+    pub async fn find_by_shorten_id(&self, shorten_id: &str) -> Result<Option<shorten_url::Model>, DbErr> {
         ShortenUrl::find()
-            .filter(ShortenUrl::shorten_id.eq(shorten_id))
+            .filter(shorten_url::Column::ShortenId.eq(shorten_id))
             .one(&self.db_conn)
             .await
     }
 
     /// Find all records with the same original URL
-    pub async fn find_all_by_original_url(&self, original_url: &str) -> Result<Vec<shorten_url::Model>> {
+    pub async fn find_all_by_original_url(&self, original_url: &str) -> Result<Vec<shorten_url::Model>, DbErr> {
         ShortenUrl::find()
-            .filter(ShortenUrl::original_url.eq(original_url))
+            .filter(shorten_url::Column::OriginalUrl.eq(original_url))
             .all(&self.db_conn)
             .await
     }
 
     /// Get all records from the database
-    pub async fn get_all(&self) -> Result<Vec<shorten_url::Model>, sea_orm::error::DbErr> {
+    pub async fn get_all(&self) -> Result<Vec<shorten_url::Model>, DbErr> {
         ShortenUrl::find()
             .all(&self.db_conn)
             .await
-            // .expect("Error while fetching all shortened URLs")
-            // .into_iter()
-            // .map(|model| model.shorten_id)
-            // .collect()
     }
 
     /// Create a new record with the given shorten ID and original URL
-    pub async fn create(&self, shorten_id: &str, original_url: &str) -> Result<()> {
+    pub async fn create(&self, shorten_id: &str, original_url: &str) -> Result<(), DbErr> {
+        // TODO: check whether the shorten ID already exists
+        let existing_shorten_url = ShortenUrl::find()
+            .filter(shorten_url::Column::ShortenId.eq(shorten_id))
+            .one(&self.db_conn)
+            .await?;
+
+        if existing_shorten_url.is_some() {
+            return Err(DbErr::Custom("Shorten ID already exists".into()));
+        }
+
+        // create a new record
         let new_shorten_url = shorten_url::ActiveModel {
-            shorten_id: Set(shorten_id.to_owned()),
-            original_url: Set(original_url.to_owned()),
+            shorten_id: ActiveValue::Set(shorten_id.to_owned()),
+            original_url: ActiveValue::Set(original_url.to_owned()),
             ..Default::default()
         };
         
-        ShortenUrl::insert(new_shorten_url)
+        let res = ShortenUrl::insert(new_shorten_url)
             .exec(&self.db_conn)
-            .await
+            .await?;
+
+        Ok(())
     }
 
     /// Delete a record with the same shorten ID
-    pub async fn delete_by_shorten_id(&self, shorten_id: &str) -> Result<()> {
+    pub async fn delete_by_shorten_id(&self, shorten_id: &str) -> Result<(), DbErr> {
         let to_be_delete: Option<shorten_url::Model> = ShortenUrl::find()
             .filter(shorten_url::Column::ShortenId.eq(shorten_id))
             .one(&self.db_conn)
             .await?;
 
-        let to_be_delete = to_be_delete.unwrap();
+        let to_be_delete: ActiveModel = to_be_delete.unwrap().into();
         ShortenUrl::delete(to_be_delete)
             .exec(&self.db_conn)
-            .await
+            .await?;
+
+        Ok(())
     }
 
     /// Delete all records with the same original URL
-    pub async fn delete_by_original_url(&self, original_url: &str) -> Result<(), sea_orm::error::ExecResult> {
+    pub async fn delete_by_original_url(&self, original_url: &str) -> Result<(), DbErr> {
         ShortenUrl::delete_many()
             .filter(shorten_url::Column::OriginalUrl.eq(original_url.to_string()))
             .exec(&self.db_conn)
-            .await
+            .await?;
+        Ok(())
     }
 }
